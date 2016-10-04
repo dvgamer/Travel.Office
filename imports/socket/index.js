cf8139bfa27b4ef69b2865c15b1cf06c59269daf
Kananek Thongkam <kem@ns.co.th>
2016-10-04 16:01:21 +0700

    Created Socket for Mbos

--- imports/socket/index.js
+++ imports/socket/index.js
@@ -1,17 +1,32 @@
 import http from 'http';
 import socket from 'socket.io';
 
-const PORT = 8131;
-var allowedOrigins = "*";
+const PORT = 81;
+
+var express = require('express');
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 
 Meteor.startup(() => {
-  const server = http.createServer();
-  const io = socket(server);
-  let counter = 0;
+  const server = http.createServer(app);
+  const io = socket(server, { log: false, origins: '*:*' });
 
   // New client
+  let counter = 0;
   io.on('connection', function(socket) {
-    console.log('new socket client');
+  	counter++;
+   	console.log(`[${counter}] SOCKET -- ${socket.id} is connected.`);
+    socket.on('disconnect', function() {
+    	counter--;
+   		console.log(`[${counter}] SOCKET -- ${this.id} disconnected.`);
+    });
   });
 
   try { server.listen(PORT); } catch (e) { console.error(e); }
--- run-deploy.cmd
+++ run-deploy.cmd
@@ -1,4 +1,6 @@
-meteor build ../ --architecture=os.linux.x86_64
+@echo off
+REM meteor build ../ --architecture=os.linux.x86_64
+PAUSE
 CD ..
-pscp UI-Mid-BackOffice.tar.gz core@ssh.touno-k.com:/app/travox-mbos/bundle.tar.gz
+pscp UI-Mid-BackOffice.tar.gz core@ssh.touno-k.com:/app/travox-mbos/UI-MidBack.tar.gz
 del UI-Mid-BackOffice.tar.gz
